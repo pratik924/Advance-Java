af4f2792815c0da4a72cfa0fe255ace87f478dfa
pratik924 <singhpariharpratik@gmail.com>
2025-09-13 15:43:02 +0530

    oky

---
+++ Advance-Java/src/com/rays/jdbc/TestInsert.java
@@ -0,0 +1,19 @@
+package com.rays.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class TestInsert {
+	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/sun", "root", "root");
+		Statement stmt = conn.createStatement();
+		int i = stmt.executeUpdate("insert into employee values(30,'uday','indore',1000000,'tcs','2')");
+		System.out.println("data inserted succesfully:" + i);
+
+	}
+
+}
--- Advance-Java/src/com/rays/jdbc/TestRemove.java
+++ Advance-Java/src/com/rays/jdbc/TestRemove.java
@@ -5,15 +5,14 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-public class TestInsert {
+public class TestRemove {
 	public static void main(String[] args) throws ClassNotFoundException, SQLException {
-		
 		Class.forName("com.mysql.cj.jdbc.Driver");
-		Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/sun","root","root");
+		Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/sun", "root", "root");
 		Statement stmt = conn.createStatement();
-		int i = stmt.executeUpdate("insert into marksheet values(21,'kunal',56,65,78),(22,'raman',78,63,69)");
-		System.out.println("data inserted succesfully:"+i);
-		
+		int i = stmt.executeUpdate("delete from employee where id =30");
+		System.out.println("deleted succesfully");
+
 	}
 
 }
